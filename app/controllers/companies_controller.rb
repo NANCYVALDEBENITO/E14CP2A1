60339ac4a82e9fdce578b3f1f08d7c7f16eb9abd
nvaldebenito <nvaldebenito@localhost.localdomain>
2019-02-06 21:49:00 -0300

    add delete employee and redirect to id company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -10,6 +10,8 @@ class CompaniesController < ApplicationController
   # GET /companies/1
   # GET /companies/1.json
   def show
+    @employee = Employee.new
+    @area = Area.new
   end
 
   # GET /companies/new
---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,17 @@
+class EmployeesController < ApplicationController
+	def create
+		@employee = Employee.new(employee_params)
+		#byebug
+		@employee.save
+		redirect_to @employee.company
+	end
+	def destroy
+		@employee = Employee.find(params[:id])
+		@employee.destroy
+		redirect_to @employee.company
+	end
+	private
+	def employee_params
+		params.require(:employee).permit(:first_name, :last_name, :email, :area_id, :company_id)
+	end
+end
